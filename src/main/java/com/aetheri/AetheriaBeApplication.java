121e294b774559d0dcdab99751e75e7dd4491a19
Duskafka <rlarlgus0206@naver.com>
2025-09-07 08:21:38 +0900

    feat: 애플리케이션이 구현된 프로퍼티 클래스를 사용하도록 등록

--- src/main/java/com/aetheri/AetheriaBeApplication.java
+++ src/main/java/com/aetheri/AetheriaBeApplication.java
@@ -1,8 +1,12 @@
 package com.aetheri;
 
+import com.aetheri.infrastructure.config.properties.JWTProperties;
+import com.aetheri.infrastructure.config.properties.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({JWTProperties.class, KakaoProperties.class})
 @SpringBootApplication
 public class AetheriaBeApplication {
 
