f6bd008d89c9600bef6c973604f2feaeb5a6900f
Duskafka <rlarlgus0206@naver.com>
2025-09-07 08:33:46 +0900

    refactor: 예외 처리 추가 및 회원가입 전 사용자가 존재하는지 확인하도록 변경함

--- src/main/java/com/aetheri/application/service/SignUpService.java
+++ src/main/java/com/aetheri/application/service/SignUpService.java
@@ -1,19 +1,39 @@
 package com.aetheri.application.service;
 
 import com.aetheri.application.port.out.r2dbc.RunnerRepositoryPort;
+import com.aetheri.domain.exception.BusinessException;
+import com.aetheri.domain.exception.message.ErrorMessage;
 import com.aetheri.infrastructure.persistence.Runner;
 import com.aetheri.interfaces.dto.kakao.KakaoUserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SignUpService {
     private final RunnerRepositoryPort runnerRepositoryPort;
 
-    public Mono<Runner> signUp(KakaoUserInfoResponseDto dto) {
-        Runner runner = new Runner(dto.id(), dto.kakaoAccount().name());
-        return runnerRepositoryPort.save(runner);
+    /**
+     * 서버 측의 회원가입을 위한 메소드.
+     *
+     * @param dto 카카오에서 받아온 사용자 정보
+     * @implNote Void로 반환하는 이유는 회원가입하고 난 엔티티를 비즈니스 로직에서 다시 사용하지 않을 것이기 때문.
+     * @implNote then을 사용하는 이유는 {@code Mono<Runner>}를 {@code Mono<Void>}로 변환하기 위함임.
+     * */
+    public Mono<Void> signUp(KakaoUserInfoResponseDto dto) {
+        return runnerRepositoryPort.existByKakaoId(dto.id())
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.error(new BusinessException(
+                                ErrorMessage.DUPLICATE_RUNNER,
+                                "이미 존재하는 사용자입니다."
+                        ));
+                    }
+                    Runner runner = new Runner(dto.id(), dto.kakaoAccount().name());
+                    return runnerRepositoryPort.save(runner).then();    // Mono<Void>로 변환하기 위해 then 사용
+                });
     }
 }
