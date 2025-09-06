2d8d353dd9af5eb0851ead542c64391bda5320d3
Duskafka <rlarlgus0206@naver.com>
2025-09-06 15:39:15 +0900

    fix: 리뷰에서 지적한 JSON 매핑 적용 (#2)

--- src/main/java/com/aetheri/interfaces/web/handler/MyHandler.java
+++ src/main/java/com/aetheri/interfaces/web/handler/MyHandler.java
@@ -2,19 +2,29 @@ package com.aetheri.interfaces.web.handler;
 
 import com.aetheri.domain.exception.BusinessException;
 import com.aetheri.domain.exception.message.ErrorMessage;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Component
 public class MyHandler {
     public Mono<ServerResponse> hello(ServerRequest request) {
-        return ServerResponse.ok().bodyValue("Hello, World!");
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("message", "Hello, World!"));
     }
 
     public Mono<ServerResponse> helloError(ServerRequest request) {
         // 의도적으로 예외 발생
-        return Mono.error(new BusinessException(ErrorMessage.INTERNAL_SERVER_ERROR, "서버에서 발생한 오류"));
+        return Mono.error(
+                new BusinessException(
+                        ErrorMessage.INTERNAL_SERVER_ERROR,
+                        "서버에서 발생한 오류"
+                )
+        );
     }
 }
