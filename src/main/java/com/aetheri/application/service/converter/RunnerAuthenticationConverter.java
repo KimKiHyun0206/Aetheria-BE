5df867dedda8c58e0f2489d7332585e2cf133df0
Duskafka <rlarlgus0206@naver.com>
2025-09-06 23:19:45 +0900

    feat(Authentication): Runner를 일반 권한을 가진 Authentication 객체로 변환하는 Converter 클래스 구현

---
+++ src/main/java/com/aetheri/application/service/converter/RunnerAuthenticationConverter.java
@@ -0,0 +1,20 @@
+package com.aetheri.application.service.converter;
+
+import com.aetheri.infrastructure.persistence.Runner;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RunnerAuthenticationConverter {
+    public static Authentication toAuthentication(Runner runner) {
+        return new UsernamePasswordAuthenticationToken(
+                runner.getId(),       // principal
+                null,                 // credentials
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
