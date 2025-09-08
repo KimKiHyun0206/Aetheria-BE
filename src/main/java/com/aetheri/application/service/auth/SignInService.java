ae081442c2b6a14303b23564bca4ee8bc2082843
Duskafka <rlarlgus0206@naver.com>
2025-09-08 17:32:02 +0900

    refactor: 리액티브 체인 리펙토링
    Mono.error를 사용하지 않도록 하였으며 리액티브 체인을 더 적극적으로 사용하도록 함.

--- src/main/java/com/aetheri/application/service/auth/SignInService.java
+++ src/main/java/com/aetheri/application/service/auth/SignInService.java
@@ -1,4 +1,4 @@
-package com.aetheri.application.service;
+package com.aetheri.application.service.auth;
 
 import com.aetheri.application.dto.SignInResponse;
 import com.aetheri.application.dto.jwt.RefreshTokenIssueResponse;
@@ -12,10 +12,8 @@ import com.aetheri.domain.exception.BusinessException;
 import com.aetheri.domain.exception.message.ErrorMessage;
 import com.aetheri.infrastructure.config.properties.JWTProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -25,10 +23,8 @@ public class SignInService {
     private final KakaoUserInformationInquiryPort kakaoUserInformationInquiryPort;
     private final RunnerRepositoryPort runnerRepositoryPort;
     private final JwtTokenProviderPort jwtTokenProviderPort;
-
     private final SignUpService signUpService;
-    private final String accessTokenHeader;
-    private final String refreshTokenCookie;
+
     private final long refreshTokenExpirationDays;
 
     public SignInService(
@@ -44,8 +40,6 @@ public class SignInService {
         this.runnerRepositoryPort = runnerRepositoryPort;
         this.jwtTokenProviderPort = jwtTokenProviderPort;
         this.signUpService = signUpService;
-        this.accessTokenHeader = jwtProperties.accessTokenHeader();
-        this.refreshTokenCookie = jwtProperties.refreshTokenCookie();
         this.refreshTokenExpirationDays = jwtProperties.refreshTokenExpirationDays();
     }
 
@@ -57,25 +51,28 @@ public class SignInService {
         );
 
         return kakaoGetAccessTokenPort
-                // 카카오 API에게서 로그인 코드로 액세스 토큰을 받아옴
+                // 액세스 토큰 가져오기
                 .tokenRequest(code)
-                // 받아온 토큰으로 사용자 정보를 카카오 API로 조회
+                // 가져온 액세스 토큰으로 사용자 정보 조회하기
                 .flatMap(token -> kakaoUserInformationInquiryPort.userInformationInquiry(token.accessToken()))
-                // 만약 사용자 조회에 실패했다면 예외 발생
+                // 만약 사용자 정보가 없다면 예외 발생
                 .switchIfEmpty(Mono.error(new BusinessException(
                         ErrorMessage.NOT_FOUND_RUNNER,
-                        "사용자 정보를 찾을 수 없습니다."
+                        "카카오에서 사용자 정보를 찾을 수 없습니다."
                 )))
-                // 사용자 조회에 성공했다면 회원가입 되어있는지 확인하고 회원가입이 되어있지 않다면 회원가입하고 되어있다면 사용자를 찾아서 반환
-                .flatMap(userInfo -> runnerRepositoryPort.existByKakaoId(userInfo.id())
-                        .flatMap(exists -> {
-                            Mono<Void> signUpMono = exists
-                                    ? Mono.empty()
-                                    : signUpService.signUp(userInfo).then();
-                            return signUpMono.then(runnerRepositoryPort.findByKakaoId(userInfo.id()));
-                        })
+                // 가져온 사용자 정보로 회원가입 및 로그인 진행
+                .flatMap(userInfo ->
+                        runnerRepositoryPort.existByKakaoId(userInfo.id())
+                                .flatMap(exists -> {
+                                    // 1. 만약 존재하지 않는다면 회원가입 Mono를 반환
+                                    if (!exists) {
+                                        return signUpService.signUp(userInfo)
+                                                .then(runnerRepositoryPort.findByKakaoId(userInfo.id()));
+                                    }
+                                    // 2. 존재한다면 바로 사용자 정보를 찾아서 반환
+                                    return runnerRepositoryPort.findByKakaoId(userInfo.id());
+                                })
                 )
-                // 찾아온 사용자를 사용하여 액세스 토큰과 리프레쉬 토큰 발급
                 .flatMap(runner -> {
                     Authentication auth = RunnerAuthenticationConverter.toAuthentication(runner);
                     String accessToken = jwtTokenProviderPort.generateAccessToken(auth);
--- src/main/java/com/aetheri/application/service/auth/SignUpService.java
+++ src/main/java/com/aetheri/application/service/auth/SignUpService.java
@@ -1,4 +1,4 @@
-package com.aetheri.application.service;
+package com.aetheri.application.service.auth;
 
 import com.aetheri.application.port.out.r2dbc.RunnerRepositoryPort;
 import com.aetheri.domain.exception.BusinessException;
@@ -24,19 +24,23 @@ public class SignUpService {
      * @implNote then을 사용하는 이유는 {@code Mono<Runner>}를 {@code Mono<Void>}로 변환하기 위함임.
      * */
     public Mono<Void> signUp(KakaoUserInfoResponseDto dto) {
+        // 1. 카카오 ID로 사용자가 존재하는지 확인
         return runnerRepositoryPort.existByKakaoId(dto.id())
-                .flatMap(exists -> {
-                    if (exists) {
-                        return Mono.error(new BusinessException(
-                                ErrorMessage.DUPLICATE_RUNNER,
-                                "이미 존재하는 사용자입니다."
-                        ));
-                    }
+                // 2. 만약 이미 존재한다면, (true -> false)가 되어 스트림이 비어버림
+                .filter(isExist -> !isExist)
+                // 3. 스트림이 비었을 때 (사용자가 존재할 때), 에러 발생
+                .switchIfEmpty(Mono.error(new BusinessException(
+                        ErrorMessage.DUPLICATE_RUNNER,
+                        "이미 존재하는 사용자입니다."
+                )))
+                // 4. 스트림이 비어있지 않을 때 (사용자가 존재하지 않을 때), 회원가입 진행
+                .flatMap(notExists -> {
                     Runner runner = new Runner(
                             dto.id(),
                             dto.properties().get("nickname")
                     );
-                    return runnerRepositoryPort.save(runner).then();    // Mono<Void>로 변환하기 위해 then 사용
-                });
+                    return runnerRepositoryPort.save(runner);
+                })
+                .then(); // Mono<Void>로 변환
     }
 }
