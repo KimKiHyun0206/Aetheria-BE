a3ab8d2e0e6622dbed158fb980f3de450a562302
Duskafka <rlarlgus0206@naver.com>
2025-09-08 21:06:07 +0900

    feat: 로그아웃 기능 구현
    - 리프레쉬 토큰 리프레쉬
    - 카카오 로그아웃
    - 서버의 카카오 토큰 삭제
    - Redis의 리프레쉬 토큰 삭제

---
+++ src/main/java/com/aetheri/application/service/sign/SignOutService.java
@@ -0,0 +1,51 @@
+package com.aetheri.application.service.sign;
+
+import com.aetheri.application.dto.KakaoTokenResponse;
+import com.aetheri.application.port.out.kakao.KakaoLogoutPort;
+import com.aetheri.application.port.out.kakao.KakaoRefreshTokenPort;
+import com.aetheri.application.port.out.r2dbc.KakaoTokenRepositortyPort;
+import com.aetheri.application.port.out.redis.RedisRefreshTokenRepositoryPort;
+import com.aetheri.infrastructure.persistence.KakaoToken;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+/**
+ * 로그아웃 서비스
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SignOutService {
+    private final KakaoTokenRepositortyPort kakaoTokenRepositortyPort;
+    private final KakaoRefreshTokenPort kakaoRefreshTokenPort;
+    private final KakaoLogoutPort kakaoLogoutPort;
+    private final RedisRefreshTokenRepositoryPort redisRefreshTokenRepositoryPort;
+
+    public Mono<Void> signOut(Long runnerId) {
+        return kakaoTokenRepositortyPort.findByRunnerId(runnerId)
+                .flatMap(this::refreshKakaoToken)
+                .flatMap(this::kakaoLogout)
+                .then(deleteKakaoToken(runnerId))
+                .then(deleteRefreshToken(runnerId))
+                .doOnSuccess(v -> log.info("[SignOutService] 로그아웃 성공: {}", runnerId));
+    }
+
+    private Mono<KakaoTokenResponse> refreshKakaoToken(KakaoToken kakaoToken) {
+        return kakaoRefreshTokenPort
+                .refreshAccessToken(kakaoToken.getRefreshToken());
+    }
+
+    private Mono<Void> kakaoLogout(KakaoTokenResponse kakaoTokenResponse) {
+        return kakaoLogoutPort.logout(kakaoTokenResponse.access_token());
+    }
+
+    private Mono<Void> deleteKakaoToken(Long runnerId) {
+        return kakaoTokenRepositortyPort.deleteByRunnerId(runnerId);
+    }
+
+    private Mono<Void> deleteRefreshToken(Long runnerId) {
+        return redisRefreshTokenRepositoryPort.deleteRefreshToken(runnerId).then();
+    }
+}
