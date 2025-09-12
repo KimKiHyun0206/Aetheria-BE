5a05f53342f92de09879d5797c904bfe3637c5ef
Duskafka <rlarlgus0206@naver.com>
2025-09-12 20:52:59 +0900

    update(#5): runner의 kakao_id에 UNIQUE 제약조건 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS kakao_token;
 CREATE TABLE runner
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
-    kakao_id BIGINT       NOT NULL,
+    kakao_id BIGINT       NOT NULL UNIQUE,
     name     VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE kakao_token
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
     runner_id     BIGINT       NOT NULL,
     access_token  VARCHAR(255) NOT NULL,
     refresh_token VARCHAR(255) NOT NULL,
