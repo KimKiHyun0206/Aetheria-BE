e91df62a82008d1be68c90e30076f15990f9b5ef
Duskafka <rlarlgus0206@naver.com>
2025-09-13 10:22:05 +0900

    fix(#5): WebClient 생성에 프로퍼티를 사용하도록 수정

--- src/main/java/com/aetheri/infrastructure/config/WebClientConfig.java
+++ src/main/java/com/aetheri/infrastructure/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.aetheri.infrastructure.config;
 
+import com.aetheri.infrastructure.config.properties.KakaoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,12 +8,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient kakaoWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder.baseUrl("https://kapi.kakao.com").build();
+    public WebClient kakaoWebClient(WebClient.Builder webClientBuilder, KakaoProperties kakaoProperties) {
+        return webClientBuilder.baseUrl(kakaoProperties.api()).build();
     }
 
     @Bean
-    public WebClient kakaoAuthWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder.baseUrl("https://kauth.kakao.com").build();
+    public WebClient kakaoAuthWebClient(WebClient.Builder webClientBuilder, KakaoProperties kakaoProperties) {
+        return webClientBuilder.baseUrl(kakaoProperties.authApi()).build();
     }
 }
