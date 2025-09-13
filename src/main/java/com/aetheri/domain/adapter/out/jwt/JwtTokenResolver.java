0e9c24fcab8711c7f165c9f7737c2407d94e63be
Duskafka <rlarlgus0206@naver.com>
2025-09-13 09:57:29 +0900

    feat(#5): 토큰 파싱 예외/형변환 예외를 도메인 예외로 변환하거나 선행 검증
    - getIdFromToken: Subject의 값이 Long이 아닌 경우 예외를 발생시킴.
    - claims: 토큰이 빈 값이라면 예외를 발생시킴.

--- src/main/java/com/aetheri/domain/adapter/out/jwt/JwtTokenResolver.java
+++ src/main/java/com/aetheri/domain/adapter/out/jwt/JwtTokenResolver.java
@@ -1,6 +1,8 @@
 package com.aetheri.domain.adapter.out.jwt;
 
 import com.aetheri.application.port.out.jwt.JwtTokenResolverPort;
+import com.aetheri.domain.exception.BusinessException;
+import com.aetheri.domain.exception.message.ErrorMessage;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import org.springframework.stereotype.Component;
@@ -18,18 +20,34 @@ public class JwtTokenResolver implements JwtTokenResolverPort {
     // 토큰에서 사용자 이름 추출
     @Override
     public Long getIdFromToken(String token) {
-        return Long.valueOf(parser
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject());
+        String sub = claims(token).getSubject();
+        try {
+            return Long.valueOf(sub);
+        } catch (NumberFormatException e) {
+            throw new BusinessException(
+                    ErrorMessage.JWT_SUBJECT_IS_NOT_NUMBER,
+                    "JWT 토큰 값이 유효하지 않습니다."
+            );
+        }
     }
 
     // 권한 가져오기
     @Override
     public List<String> getRolesFromToken(String token) {
-        Claims claims = parser.parseClaimsJws(token).getBody();
+        List<?> roles = claims(token).get("roles", List.class);
 
-        List<?> roles = claims.get("roles", List.class);
-        return roles == null ? List.of() : roles.stream().map(String::valueOf).toList();
+        return roles == null ?
+                List.of() :
+                roles.stream().map(String::valueOf).toList();
+    }
+
+    private Claims claims(String token) {
+        if (token == null || token.isBlank()) {
+            throw new BusinessException(
+                    ErrorMessage.JWT_TOKEN_IS_EMPTY,
+                    "JWT 토큰이 비어있습니다."
+            );
+        }
+        return parser.parseClaimsJws(token).getBody();
     }
 }
