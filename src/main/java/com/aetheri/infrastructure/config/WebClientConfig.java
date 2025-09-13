e0b06d2f9a0f36f028a02680b9998643fd38d481
Duskafka <rlarlgus0206@naver.com>
2025-09-13 11:27:26 +0900

    feat(#5): 외부 API WebClient에 타임아웃과 네트워크 옵션 설정
    외부 API 사용 시 타임아웃이 생기도록 네트워크 옵션 설정

--- src/main/java/com/aetheri/infrastructure/config/WebClientConfig.java
+++ src/main/java/com/aetheri/infrastructure/config/WebClientConfig.java
@@ -1,19 +1,44 @@
 package com.aetheri.infrastructure.config;
 
 import com.aetheri.infrastructure.config.properties.KakaoProperties;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
     @Bean
     public WebClient kakaoWebClient(WebClient.Builder webClientBuilder, KakaoProperties kakaoProperties) {
-        return webClientBuilder.baseUrl(kakaoProperties.api()).build();
+        return webClientBuilder.baseUrl(kakaoProperties.api())
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                                .responseTimeout(Duration.ofSeconds(5))
+                                .doOnConnected(c -> c
+                                        .addHandlerLast(new ReadTimeoutHandler(5))
+                                        .addHandlerLast(new WriteTimeoutHandler(5)))
+                ))
+                .build();
     }
 
     @Bean
     public WebClient kakaoAuthWebClient(WebClient.Builder webClientBuilder, KakaoProperties kakaoProperties) {
-        return webClientBuilder.baseUrl(kakaoProperties.authApi()).build();
+        return webClientBuilder.baseUrl(kakaoProperties.authApi())
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                                .responseTimeout(Duration.ofSeconds(5))
+                                .doOnConnected(c -> c
+                                        .addHandlerLast(new ReadTimeoutHandler(5))
+                                        .addHandlerLast(new WriteTimeoutHandler(5)))
+                ))
+                .build();
     }
 }
