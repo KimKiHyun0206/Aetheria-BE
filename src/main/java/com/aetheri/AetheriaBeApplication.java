1864bc66bd0108dffd9344c7a9baf5dda0af5f35
Duskafka <rlarlgus0206@naver.com>
2025-09-22 15:32:10 +0900

    feat(#6): 이미지 프로퍼티 추가

--- src/main/java/com/aetheri/AetheriaBeApplication.java
+++ src/main/java/com/aetheri/AetheriaBeApplication.java
@@ -1,12 +1,17 @@
 package com.aetheri;
 
+import com.aetheri.infrastructure.config.properties.ImageProperties;
 import com.aetheri.infrastructure.config.properties.JWTProperties;
 import com.aetheri.infrastructure.config.properties.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties({JWTProperties.class, KakaoProperties.class})
+@EnableConfigurationProperties({
+        JWTProperties.class,
+        KakaoProperties.class,
+        ImageProperties.class
+})
 @SpringBootApplication
 public class AetheriaBeApplication {
 
