6a1b5a455a99abf4d00734f8cda862b854531909
Duskafka <rlarlgus0206@naver.com>
2025-09-24 10:00:23 +0900

    docs(#10): 이미지 레포지토리 포트 문서화

--- src/main/java/com/aetheri/application/port/out/image/ImageRepositoryPort.java
+++ src/main/java/com/aetheri/application/port/out/image/ImageRepositoryPort.java
@@ -6,12 +6,28 @@ import com.aetheri.infrastructure.persistence.entity.ImageMetadata;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+/**
+ * 이미지 메타데이터를 데이터베이스에서 조작하기 위한 포트
+ * */
 public interface ImageRepositoryPort {
+    // 이미지 메타데이터를 등록하기 위한 메소드
     Mono<Long> saveImageMetadata(ImageMetadataSaveDto dto);
+
+    // 이미지 메타데이터를 ID로 조회하기 위한 메소드
     Mono<ImageMetadata> findById(Long imageId);
+
+    // 이미지 메타데이터를 사용자 ID로 조회하기 위한 메소드
     Flux<ImageMetadata> findByRunnerId(Long runnerId);
+
+    // 이미지 메타데이터를 수정하기 위한 메소드
     Mono<Long> updateImageMetadata(Long runnerId, Long imageId, ImageMetadataUpdateRequest request);
+
+    // 이미지 메타데이터가 존재하는지 확인하기 위한 메소드
     Mono<Boolean> isExistImageMetadata(Long imageId);
+
+    // 이미지 메타데이터를 사용자 ID와 이미지 ID를 사용해서 삭제하기 위한 메소드
     Mono<Long> deleteById(Long runnerId, Long imageId);
+
+    // 이미지 메타데이터를 사용자 ID를 사용해서 삭제하기 위한 메소드
     Mono<Long> deleteByRunnerId(Long runnerId);
 }
