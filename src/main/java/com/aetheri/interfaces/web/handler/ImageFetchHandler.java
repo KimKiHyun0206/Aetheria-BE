bd77f7dc5912600e3064509fe2a336aab3b3fd91
Duskafka <rlarlgus0206@naver.com>
2025-09-28 13:23:24 +0900

    feat(#9): 이미지를 캐싱할 수 있도록 헤더에 정보 추가

--- src/main/java/com/aetheri/interfaces/web/handler/ImageFetchHandler.java
+++ src/main/java/com/aetheri/interfaces/web/handler/ImageFetchHandler.java
@@ -13,7 +13,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ImgFetchHandler {
+public class ImageFetchHandler {
     private final ImageFetchUseCase imageFetchUseCase;
 
     public Mono<ServerResponse> imageFetch(ServerRequest serverRequest) {
@@ -21,8 +21,11 @@ public class ImgFetchHandler {
         Mono<Resource> imageMono = imageFetchUseCase.fetchImage(path);
 
         return ServerResponse.ok()
-                // 이미지 타입에 맞게 MediaType 설정 (예: JPEG)
+                // 이미지 타입에 맞게 MediaType 설정
+                // TODO 추후 비트맵 이미지를 저장하고 통신할 수 있도록 함
                 .contentType(MediaType.IMAGE_JPEG)
+                // 1시간 동안 이미지를 캐싱할 수 있도록 함.
+                .header("Cache-Control", "public, max-age=3600") // 1시간 캐시
                 // body()를 사용하여 Mono<T>를 응답 본문으로 스트리밍
                 .body(imageMono, Resource.class);
     }
