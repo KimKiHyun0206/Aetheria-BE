d269cf2c1d8abf2b7b4950168eac3e8448c71462
Duskafka <rlarlgus0206@naver.com>
2025-09-30 14:57:43 +0900

    chore(#18): TODO 작성

--- src/main/java/com/aetheri/application/dto/MeResponse.java
+++ src/main/java/com/aetheri/application/dto/MeResponse.java
@@ -3,6 +3,7 @@ package com.aetheri.application.dto;
 import com.aetheri.infrastructure.persistence.entity.Runner;
 
 public record MeResponse(String name) {
+    // TODO Domain 엔티티를 직접 참조하고 있는데, 이는 레이어 규칙을 위반한 것임
     public static MeResponse of(Runner runner) {
         return new MeResponse(runner.getName());
     }
